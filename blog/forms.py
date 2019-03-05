890874285f41759ed8a3326449996850af96311a
dydwkd486 <dydwkd48670@gmail.com>
2019-03-05 13:05:16 +0900

    first commit

--- blog/forms.py
+++ blog/forms.py
@@ -2,10 +2,17 @@ from django import forms
 from .models import Animal_map
 
 class DateInput(forms.DateInput):
-    input_type = 'datetime'
+    input_type = 'datetime_local'
 
 class DateStep(forms.DateInput):
     type = 'text'
+
+class Dateimgfile(forms.FileInput):
+    accept='.gif, .jpg, .png'
+
+class Datesoundfile(forms.FileInput):
+    accept='.mp3,.wav'
+
 
 class Animal_mapForm(forms.ModelForm):
 
@@ -14,9 +21,8 @@ class Animal_mapForm(forms.ModelForm):
         fields = ('title', 'Latitude', 'Longitude', 'content','observed_date','soundfile','file')
         widgets = {
             'observed_date': DateInput(),
-            'Latitude':DateStep,
-            'Longitude':DateStep
-
+            'Latitude':DateStep(),
+            'Longitude':DateStep(),            
         }
 
     def __init__(self, *args, **kwargs):
